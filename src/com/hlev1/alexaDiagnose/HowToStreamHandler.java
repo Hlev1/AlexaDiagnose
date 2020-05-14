68a7a177e208cde2863c57706295cac77a3ec1dc
Harry Levick <harrylevick@hotmail.co.uk>
2020-05-14 09:32:07 +0100

    Update HowToStreamHandler.java

--- src/com/hlev1/alexaDiagnose/HowToStreamHandler.java
+++ src/com/hlev1/alexaDiagnose/HowToStreamHandler.java
@@ -1,1 +1,35 @@
+package com.hlev1.alexaDiagnose;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+import com.hlev1.alexaDiagnose.handlers.ErrorHandler;
+import com.hlev1.alexaDiagnose.handlers.ExitIntentHandler;
+import com.hlev1.alexaDiagnose.handlers.HelpIntentHandler;
+import com.hlev1.alexaDiagnose.handlers.LaunchHandler;
+import com.hlev1.alexaDiagnose.handlers.RecipeIntentHandler;
+import com.hlev1.alexaDiagnose.handlers.RepeatIntentHandler;
+import com.hlev1.alexaDiagnose.handlers.SessionEndedHandler;
+
+public class HowToStreamHandler extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(
+                        new LaunchHandler(),
+                        new HelpIntentHandler(),
                         new RecipeIntentHandler(),
+                        new RepeatIntentHandler(),
+                        new ExitIntentHandler(),
+                        new ErrorHandler(),
+                        new SessionEndedHandler()
+                )
+                // Add your skill id below
+                // .withSkillId("")
+                .build();
+    }
+
+    public HowToStreamHandler() {
+        super(getSkill());
+    }
+}
