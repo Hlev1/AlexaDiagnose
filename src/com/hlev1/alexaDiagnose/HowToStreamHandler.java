1da7a7a6362171974a58ea62f1a943f9edb055c2
Harry Levick <harrylevick@hotmail.co.uk>
2020-05-15 21:58:55 +0100

    Update HowToStreamHandler.java

--- src/com/hlev1/alexaDiagnose/HowToStreamHandler.java
+++ src/com/hlev1/alexaDiagnose/HowToStreamHandler.java
@@ -3,18 +3,23 @@ package com.hlev1.alexaDiagnose;
 import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDB;
 import com.hlev1.alexaDiagnose.handlers.*;
 
 public class HowToStreamHandler extends SkillStreamHandler {
 
+
     private static Skill getSkill() {
+
         return Skills.standard()
                 .addRequestHandlers(
                         new LaunchHandler(),
-                        new ProfileSelectorIntentHandler(),
-                        new CreateProfileIntentHandler(),
+                        //new ProfileSelectorIntentHandler(),
+                        //new CreateProfileIntentHandler(),
                         new HelpIntentHandler(),
-                        new RecipeIntentHandler(),
                         new AgeIntentHandler(),
                         new BeginDiagnosisIntentHandler(),
                         new RepeatIntentHandler(),
@@ -26,6 +31,7 @@ public class HowToStreamHandler extends SkillStreamHandler {
                 // .withSkillId("")
                 //.withTableName("profiles")
                 //.withAutoCreateTable(true)
+                //.withDynamoDbClient(AmazonDynamoDBClientBuilder.standard().build())
                 .build();
     }
 
