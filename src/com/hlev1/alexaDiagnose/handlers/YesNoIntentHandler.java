339c41af0cfcfa8855e1adaa2518c56af694f2b3
Harry Levick <harrylevick@hotmail.co.uk>
2020-05-18 21:53:02 +0100

    get previous question asked and put answer into evidence list

--- src/com/hlev1/alexaDiagnose/handlers/YesNoIntentHandler.java
+++ src/com/hlev1/alexaDiagnose/handlers/YesNoIntentHandler.java
@@ -20,3 +20,33 @@ public class YesNoIntentHandler implements IntentRequestHandler {
         return intentRequest.getIntent().getName().equals("AMAZON.YesIntent") ||
                 intentRequest.getIntent().getName().equals("AMAZON.NoIntent");
     }
+
+    @Override
+    public Optional<Response> handle(HandlerInput handlerInput, IntentRequest intentRequest) {
+        Map session = handlerInput.getAttributesManager().getSessionAttributes();
+
+        // If the session storage contains a just asked question id. This intent handler has
+        // been caught because the user has replied to a question. We must record the answer
+        // to the question and check to see if we need to ask another question.
+
+        // Ask another question
+        ArrayList listOfQuestions = (ArrayList) session.get(CONTINUOUS_QUESTION);
+
+        if (listOfQuestions.size() == 0) {
+            session.remove(CONTINUOUS_QUESTION);
+            BeginDiagnosisIntentHandler switchIntent = new BeginDiagnosisIntentHandler();
+            return switchIntent.handle(handlerInput, intentRequest);
+        }
+
+        LinkedHashMap nextQuestion = (LinkedHashMap) listOfQuestions.remove(0);
+
+        String nextQuestionText = (String) nextQuestion.get("name");
+        String nextQuestionId = (String) nextQuestion.get("id");
+        session.put(JUST_ASKED, nextQuestionId);
+
+        return handlerInput.getResponseBuilder()
+                .withSpeech(nextQuestionText)
+                .build();
+
+    }
+}
