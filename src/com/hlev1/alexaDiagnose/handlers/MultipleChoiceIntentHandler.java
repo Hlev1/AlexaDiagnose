3dffe5e33f87c99dc9fa23e3d42a1e43d851bfaf
Harry Levick <harrylevick@hotmail.co.uk>
2020-05-23 15:17:40 +0100

    Add error handling to MultipleChoiceIntentHandler

--- src/com/hlev1/alexaDiagnose/handlers/MultipleChoiceIntentHandler.java
+++ src/com/hlev1/alexaDiagnose/handlers/MultipleChoiceIntentHandler.java
@@ -23,15 +23,24 @@ public class MultipleChoiceIntentHandler implements IntentRequestHandler {
     public Optional<Response> handle(HandlerInput handlerInput, IntentRequest intentRequest) {
         Map session = handlerInput.getAttributesManager().getSessionAttributes();
         ArrayList evidence = (ArrayList) session.getOrDefault(EVIDENCE, new ArrayList());
-
         Slot slot = intentRequest.getIntent().getSlots().get("answer");
         ArrayList question = (ArrayList) session.get(CONTINUOUS_QUESTION);
-        int answer = Integer.parseInt(slot.getValue()) - 1;
+        LinkedHashMap newEvidence = null;
+        try {
+            int answer = Integer.parseInt(slot.getValue()) - 1;
+            newEvidence = (LinkedHashMap) question.get(answer);
+            newEvidence.remove("name");
+            newEvidence.put("choice_id", "present");
+            if (newEvidence.containsKey("explanation")) { newEvidence.remove("name"); }
 
-        LinkedHashMap newEvidence = (LinkedHashMap) question.get(answer);
-        newEvidence.remove("name");
-        newEvidence.put("choice_id", "present");
-        if (newEvidence.containsKey("explanation")) { newEvidence.remove("name"); }
+        } catch (Exception e) {
+            String justAsked = (String) session.get(JUST_ASKED_Q);
+            return handlerInput.getResponseBuilder()
+                    .withSpeech("That didn't work, could you try again please.")
+                    .withReprompt(justAsked)
+                    .build();
+        }
+
 
         evidence.add(newEvidence);
         // if the evidence.size() is greater than 1, that means that there was previous evidence saved.
